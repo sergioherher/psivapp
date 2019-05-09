6ef4deec78bd82dfbbaed3d616f7b2298b018c91
sergioherher <sergiovm@facilposcorreo@gmail.com>
2019-05-09 00:16:49 -0400

    Configuracion para validacion por parte del usuario

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,5 +1,6 @@
 <?php
-    $conn = new mysqli('localhost','root','zarathustra','psivapp');
+    //$conn = new mysqli('localhost','root','zarathustra','psivapp');
+	$conn = new mysqli('by6oiyh4lg6qxaniff6m-mysql.services.clever-cloud.com','unkr5cm5vhkghdqxnpaj','FMhWGZPXiEJ45XexoGWb','by6oiyh4lg6qxaniff6m');
 
     if ($conn->connect_error) {
       echo $conn->connect_error;
