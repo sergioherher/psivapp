9a33bc9ecd733e1fdd233da37bea53d0a9b163aa
sergioherher <sergiovm@facilposcorreo@gmail.com>
2019-07-12 12:11:50 -0400

    Inicio de redisenio

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
+        <title>PSIVA | Elevando tu calidad de vida</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -14,7 +14,8 @@
         <link rel="stylesheet" href="css/fontawesome-all.min.css">
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" /> -->
+        <script rel="stylesheet" href="css/materialize.min.css"/>
         <?php
             $archivo = basename($_SERVER['PHP_SELF']);
             $pagina = str_replace(".php", "", $archivo);
@@ -29,54 +30,59 @@
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body class="<?php echo $pagina; ?>">
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <header class="site-header">
-    <div class="hero">
-      <div class="contenido-header">
-          <nav class="redes-sociales">
-            <a href="https://www.facebook.com/PsicologiaIntegralconValores" target="_blank"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
-            <a href="https://twitter.com/psicinesmejia" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i></a>
-            <a href="https://www.youtube.com/user/psicisne" target="_blank"><i class="fab fa-youtube" aria-hidden="true"></i></a>
-            <a href="https://api.whatsapp.com/send?phone=523312924902&amp;text=Hola,%20necesito%20información%20sobre%20sus%20servicios%20psicologicos" target="_blank"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
-          </nav>
-          <div class="informacion-evento">
-            <div class="clearfix">
-            <p class="fecha"><i class="fas fa-calendar-alt" aria-hidden="true"></i>09 Junio 2019</p>
-            <p class="ciudad"><i class="fas fa-map-marker-alt"aria-hidden="true"></i>Guadalajara,MX</p>
+    <!-- Add your site or application content here -->
+    <!-- 
+    <header class="site-header">
+      <div class="hero">
+        <div class="contenido-header">
+
+            <nav class="redes-sociales">
+
+              <a href="https://www.facebook.com/PsicologiaIntegralconValores" target="_blank"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+              <a href="https://twitter.com/psicinesmejia" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+              <a href="https://www.youtube.com/user/psicisne" target="_blank"><i class="fab fa-youtube" aria-hidden="true"></i></a>
+              <a href="https://api.whatsapp.com/send?phone=523312924902&amp;text=Hola,%20necesito%20información%20sobre%20sus%20servicios%20psicologicos" target="_blank"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
+            </nav>
+
+            <div class="informacion-evento">
+              <div class="clearfix">
+              <p class="fecha"><i class="fas fa-calendar-alt" aria-hidden="true"></i>09 Junio 2019</p>
+              <p class="ciudad"><i class="fas fa-map-marker-alt"aria-hidden="true"></i>Guadalajara,MX</p>
+            </div>
+
+            <h1 class="nombre-sitio">PSIVAPP</h1>
+            <p class="slogan">Elevando tu <span>Calidad de Vida</span></p>
+            </div>
+
+        </div>
+      </div>
+    </header >
+    -->
+
+    <!-- <div class="barra">
+      <div class="contenedor clearfix">
+        <div class="logo">
+          <a href="index.php">
+          <img src="img/psiva.jpg" alt="Logo PSIVAPP">
+        </a>
+        </div>
+          <div class="menu-movil">
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
-
-          <h1 class="nombre-sitio">PSIVAPP</h1>
-          <p class="slogan">Elevando tu <span>Calidad de Vida</span></p>
-          </div><!--.informacion-evento-->
-
+          <nav class="navegacion-principal clearfix">
+            <a href="conferencia.php">Conferencias</a>
+            <a href="calendario.php">Calendario</a>
+            <a href="invitados.php">Invitados</a>
+            <?php //if(isset($_SESSION['usuario_cliente'])) { ?>
+            <a href="login.php?cerrar_sesion=true"><i class="fa fa-user"></i> Cerrar Sesión</a>
+            <?php } ?>
+            <a href="login.php">Reservaciones</a>
+          </nav>
       </div>
-  </div><!--hero-->
-
-</header >
-<div class="barra">
-  <div class="contenedor clearfix">
-    <div class="logo">
-      <a href="index.php">
-      <img src="img/psiva.jpg" alt="Logo PSIVAPP">
-    </a>
-    </div>
-      <div class="menu-movil">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <nav class="navegacion-principal clearfix">
-        <a href="conferencia.php">Conferencias</a>
-        <a href="calendario.php">Calendario</a>
-        <a href="invitados.php">Invitados</a>
-        <?php if(isset($_SESSION['usuario_cliente'])) { ?>
-        <a href="login.php?cerrar_sesion=true"><i class="fa fa-user"></i> Cerrar Sesión</a>
-        <?php } ?>
-        <a href="login.php">Reservaciones</a>
-      </nav>
-  </div><!--.contenedor-->
-</div>
+    </div> -->
